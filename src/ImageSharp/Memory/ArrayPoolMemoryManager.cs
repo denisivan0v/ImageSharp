bda8e0e76d45574c764b4b9b7068525de150839f
denisivan0v <denis@ivanovdenis.ru>
2018-01-15 10:56:52 +0700

    xml docs fix

--- src/ImageSharp/Memory/ArrayPoolMemoryManager.cs
+++ src/ImageSharp/Memory/ArrayPoolMemoryManager.cs
@@ -1,7 +1,5 @@
-﻿﻿using System;
-using System.Buffers;
+﻿using System.Buffers;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 namespace SixLabors.ImageSharp.Memory
 {
@@ -28,8 +26,8 @@ namespace SixLabors.ImageSharp.Memory
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ArrayPoolMemoryManager"/> class.
-        /// <param name="maxPoolSizeInBytes">The maximum size of pooled arrays. Arrays over the thershold are gonna be always allocated.</param>
         /// </summary>
+        /// <param name="maxPoolSizeInBytes">The maximum size of pooled arrays. Arrays over the thershold are gonna be always allocated.</param>
         public ArrayPoolMemoryManager(int maxPoolSizeInBytes)
         {
             Guard.MustBeGreaterThan(maxPoolSizeInBytes, 0, nameof(maxPoolSizeInBytes));
