25b3458a2d61e17802ad73bf4e6463e7f678abd2
denisivan0v <denis@ivanovdenis.ru>
2018-01-24 19:46:42 +0700

    disabling of LargeArrayPool usage

--- src/ImageSharp/Memory/PixelDataPool{T}.cs
+++ src/ImageSharp/Memory/PixelDataPool{T}.cs
@@ -20,24 +20,24 @@ namespace SixLabors.ImageSharp.Memory
         internal const int MaxPooledBufferSizeInBytes = 32 * 1024 * 1024;
 
         /// <summary>
-        /// The threshold to pool arrays in <see cref="LargeArrayPool"/> which has less buckets for memory safety.
+        /// The threshold to pool arrays in <see cref="NormalArrayPool"/> which has less buckets for memory safety.
         /// </summary>
         private const int LargeBufferThresholdInBytes = 8 * 1024 * 1024;
 
-        /// <summary>
-        /// The maximum array length of the <see cref="LargeArrayPool"/>.
-        /// </summary>
-        private static readonly int MaxLargeArrayLength = MaxPooledBufferSizeInBytes / Unsafe.SizeOf<T>();
+//        /// <summary>
+//        /// The maximum array length of the <see cref="LargeArrayPool"/>.
+//        /// </summary>
+//        private static readonly int MaxLargeArrayLength = MaxPooledBufferSizeInBytes / Unsafe.SizeOf<T>();
 
         /// <summary>
         /// The maximum array length of the <see cref="NormalArrayPool"/>.
         /// </summary>
         private static readonly int MaxNormalArrayLength = LargeBufferThresholdInBytes / Unsafe.SizeOf<T>();
 
-        /// <summary>
-        /// The <see cref="ArrayPool{T}"/> for huge buffers, which is not kept clean.
-        /// </summary>
-        private static readonly ArrayPool<T> LargeArrayPool = ArrayPool<T>.Create(MaxLargeArrayLength, 1);
+//        /// <summary>
+//        /// The <see cref="ArrayPool{T}"/> for huge buffers, which is not kept clean.
+//        /// </summary>
+//        private static readonly ArrayPool<T> LargeArrayPool = ArrayPool<T>.Create(MaxLargeArrayLength, 1);
 
         /// <summary>
         /// The <see cref="ArrayPool{T}"/> for small-to-medium buffers which is not kept clean.
@@ -51,14 +51,14 @@ namespace SixLabors.ImageSharp.Memory
         /// <returns>The <see cref="T:TPixel[]"/></returns>
         public static T[] Rent(int minimumLength)
         {
-            if (minimumLength <= MaxNormalArrayLength)
-            {
+//            if (minimumLength <= MaxNormalArrayLength)
+//            {
                 return NormalArrayPool.Rent(minimumLength);
-            }
-            else
-            {
-                return LargeArrayPool.Rent(minimumLength);
-            }
+//            }
+//            else
+//            {
+//                return LargeArrayPool.Rent(minimumLength);
+//            }
         }
 
         /// <summary>
@@ -67,14 +67,14 @@ namespace SixLabors.ImageSharp.Memory
         /// <param name="array">The array to return to the buffer pool.</param>
         public static void Return(T[] array)
         {
-            if (array.Length <= MaxNormalArrayLength)
-            {
+//            if (array.Length <= MaxNormalArrayLength)
+//            {
                 NormalArrayPool.Return(array);
-            }
-            else
-            {
-                LargeArrayPool.Return(array);
-            }
+//            }
+//            else
+//            {
+//                LargeArrayPool.Return(array);
+//            }
         }
     }
 }
