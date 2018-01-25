12931556b81e13e206fa3c68f05e5afbf2db9120
denisivan0v <denis@ivanovdenis.ru>
2018-01-25 15:07:25 +0700

    ArrayPool<T>.Create(1 * 1024 * 1024, 10)

--- src/ImageSharp/Memory/PixelDataPool{T}.cs
+++ src/ImageSharp/Memory/PixelDataPool{T}.cs
@@ -19,24 +19,22 @@ namespace SixLabors.ImageSharp.Memory
         /// </summary>
         internal const int MaxPooledBufferSizeInBytes = 32 * 1024 * 1024;
 
+/*
         /// <summary>
         /// The threshold to pool arrays in <see cref="NormalArrayPool"/> which has less buckets for memory safety.
         /// </summary>
         private const int LargeBufferThresholdInBytes = 8 * 1024 * 1024;
 
-/*
         /// <summary>
         /// The maximum array length of the <see cref="LargeArrayPool"/>.
         /// </summary>
         private static readonly int MaxLargeArrayLength = MaxPooledBufferSizeInBytes / Unsafe.SizeOf<T>();
-*/
 
         /// <summary>
         /// The maximum array length of the <see cref="NormalArrayPool"/>.
         /// </summary>
         private static readonly int MaxNormalArrayLength = LargeBufferThresholdInBytes / Unsafe.SizeOf<T>();
 
-/*
         /// <summary>
         /// The <see cref="ArrayPool{T}"/> for huge buffers, which is not kept clean.
         /// </summary>
@@ -46,7 +44,7 @@ namespace SixLabors.ImageSharp.Memory
         /// <summary>
         /// The <see cref="ArrayPool{T}"/> for small-to-medium buffers which is not kept clean.
         /// </summary>
-        private static readonly ArrayPool<T> NormalArrayPool = ArrayPool<T>.Create(MaxNormalArrayLength, 2);
+        private static readonly ArrayPool<T> NormalArrayPool = ArrayPool<T>.Create(1 * 1024 * 1024, 10);
 
         /// <summary>
         /// Rents the pixel array from the pool.
